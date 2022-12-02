6b2b69930098e396f7a617fc51533c5ff65088f5
Aisyahmsp <84187256+Aisyahmsp@users.noreply.github.com>
2022-12-02 19:05:45 +0700

    Update hepatitis.py

--- hepatitis.py
+++ hepatitis.py
@@ -1,17 +1,23 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
+import numpy as np
+from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from collections import OrderedDict
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, f1_score
 from sklearn.neighbors import KNeighborsClassifier
+from numpy import array
+from sklearn import tree
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, f1_score
 from sklearn.tree import DecisionTreeClassifier
+from collections import OrderedDict
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import BaggingClassifier
 from sklearn.datasets import make_classification
 from sklearn.svm import SVC
+import altair as alt
+from sklearn.utils.validation import joblib
 
 st.write(""" 
 # WELCOME TO PREDICT HEPATITIS C SYSTEM
