11605fd3d04cf60473a8272fa56690430c40d7cb
Aisyahmsp <84187256+Aisyahmsp@users.noreply.github.com>
2022-12-02 16:41:56 +0700

    Update hepatitis.py

--- hepatitis.py
+++ hepatitis.py
@@ -25,7 +25,17 @@ with tab1:
     st.write("HEPATITIS C DATASET")
     st.write("Dataset ini berisi nilai laboratorium donor darah dan pasien Hepatitis C serta nilai demografi seperti usia")
     st.write("Data ini digunakan untuk memodelkan penyakit Hepatitis C")
-    st.write("Fitur - Fitur dalam dataset ini yang akan digunakan ialah...")
+    st.write("Fitur - Fitur dalam dataset ini yang akan digunakan ialah sebagai berikut.\n
+            1. ALB (Albumin) \n
+            2. ALP (Alkaline Phosphatase) \n
+            3. ALT (Alanine Transaminase) \n
+            4. AST (Aspartate aminotransferase) \n
+            5. BIL (Bilirubin) \n
+            6. CHE (Cholinesterase) \n
+            7. CHOL (Cholesterol) \n
+            8. CREA (Creatin) \n
+            9. GGT (Gamma-glutamyl transferase) \n
+            10. PROT (Protein)")
     st.write("Sumber Data : https://www.kaggle.com/datasets/fedesoriano/hepatitis-c-dataset")
 
 with tab2:
@@ -35,4 +45,14 @@ with tab2:
         df = pd.read_csv(uploaded_file)
         st.write("Nama File Anda = ", uploaded_file.name)
         st.dataframe(df)
-
+        
+with tab3 : 
+    st.write("""# Preprocessing""")
+    df.drop('Unnamed: 0',axis=1,inplace=True) 
+    df.replace(to_replace=['0=Blood Donor', '0s=suspect Blood Donor', '1=Hepatitis',
+       '2=Fibrosis', '3=Cirrhosis'],
+             value=['0','1','2','3','4'],inplace=True)
+    df['Category'].unique()
+    df.replace(to_replace=['m','f'],
+             value=['0','1'],inplace=True)
+    st.dataframe(df)
